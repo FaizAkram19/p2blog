0789b09b86c6a8b96a79b995146227d716fd8a13
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-04-02 18:31:50 +0000

    Created the form and refined the views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.db.models import F
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
@@ -13,8 +15,24 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question":question})
 
 def result(request, question_id):
-    return HttpResponse("You are looking at the results for %s" %question_id)
+    question=get_object_or_404(Question, question_id)
+    return render(request, "polls/result.html", {"question":question})
 
 def vote(request, question_id):
-    return HttpResponse("You are voting for question %s" %question_id)
+    question=get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST["choice"])
+    except(KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question":question,
+                "error":"You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes= F("votes")+1
+        selected_choice.save()
 
+        return HttpResponseRedirect(reverse("polls:result", args=(question.id, )))
