3f48de1cf9a4d1876ba4b094757bd322ad7fcacf
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-04-03 17:17:24 +0000

    Creating tests.

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+import datetime
+from .models import Question
 
 # Create your tests here.
+
+def create_question(question_text, days):
+    return Question.objects.create(timezone.now()+datetime.timedelta(days=days))
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        future_question=Question.objects.create(question_text="future question", pub_date=timezone.now()+datetime.timedelta(days=30))
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        old_question=Question.objects.create(question_text="old question", pub_date=timezone.now()-datetime.timedelta(days=1, seconds=1))
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently(self):
+        recent_question=Question.objects.create(question_text="recent question", pub_date=timezone.now()-datetime.timedelta(hours=23, minutes=59, seconds=59))
+        self.assertIs(recent_question.was_published_recently(), True)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_question(self):
+        response=self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from .models import Question, Choice
 from django.db.models import F
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 # Create your views here.
 
 class IndexView(generic.ListView):
@@ -11,7 +12,7 @@ class IndexView(generic.ListView):
     context_object_name="latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
         
 
 """def index(request):
